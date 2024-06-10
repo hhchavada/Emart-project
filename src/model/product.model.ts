3eccf1739a31426a0af213ea960f9a7b463d00af
hhchavada <harshchavada@174gmail.com>
2024-06-09 22:25:06 -0700

    express.js

---
+++ src/model/product.model.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        unique: true
+    },
+    description: {
+        type: String
+    },
+    price: {
+        type: Number
+    },
+    category: [{
+        type: String
+    }],
+    isDelete: {
+        type: Boolean,
+        default: false
+    }
+},
+{
+    versionKey: false,
+    timestamps: true
+});
+
+const productModel =  mongoose.model('products', productSchema);
+export default productModel
---
+++ src/routes/admin/admin.routes.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import { adminVerifyToken } from "../../helpers/adminVerifyToken";
+
+import { registerAdmin, 
+    loginAdmin, 
+    getAllAdmin, 
+    getAdmin, 
+    updateAdmin, 
+    deleteAdmin, 
+    updatePassword } from "../../controller/admin/admin.controller";
+
+const adminRoutes = express.Router();
+
+// REGISTER ADMIN
+adminRoutes.post("/register-Admin" , registerAdmin);
+
+// LOGIN ADMIN
+adminRoutes.post("/login-Admin" , loginAdmin);
+
+// GET ALL ADMIN
+adminRoutes.get("/get-All-Admin" , adminVerifyToken , getAllAdmin);
+
+// GET SPECIFIC ADMIN
+adminRoutes.get("/get-Admin" , adminVerifyToken , getAdmin);
+
+// UPDATE ADMIN
+adminRoutes.put("/update-Admin" , adminVerifyToken , updateAdmin);
+
+// DELETE ADMIN
+adminRoutes.delete("/delete-Admin" , adminVerifyToken , deleteAdmin);
+
+// UPDATE PASSWORD
+adminRoutes.put("/update-Password" , adminVerifyToken , updatePassword);
+
+
+export default adminRoutes;
